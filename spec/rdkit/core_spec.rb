95bff6ea62146636d08d3369f20a91116e96e997
Forrest Ye <afu@forresty.com>
2015-05-30 15:19:16 +0800

    fix spec

--- spec/rdkit/core_spec.rb
+++ spec/rdkit/core_spec.rb
@@ -2,14 +2,7 @@ require 'spec_helper'
 
 module RDKit
   describe Core do
-    it { is_expected.to respond_to :introspection }
     it { is_expected.to respond_to :tick! }
-
-    describe '#introspection' do
-      it 'raises NotImplementedError' do
-        expect { subject.introspection }.to raise_exception(ShouldOverrideError)
-      end
-    end
 
     describe 'class methods' do
       subject { Core }

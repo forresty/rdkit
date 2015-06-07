a5bd8f2c3540afc15942a369112d00a8c03813f6
Forrest Ye <afu@forresty.com>
2015-06-08 03:55:20 +0800

    more spec

--- spec/rdkit/resp_runner_spec.rb
+++ spec/rdkit/resp_runner_spec.rb
@@ -4,6 +4,14 @@ module RDKit
   describe RESPRunner do
     it { is_expected.to respond_to :resp }
 
+    it { is_expected.to respond_to :info }
+    it { is_expected.to respond_to :ping }
+    it { is_expected.to respond_to :select }
+    it { is_expected.to respond_to :echo }
+    it { is_expected.to respond_to :time }
+    it { is_expected.to respond_to :config }
+    it { is_expected.to respond_to :slowlog }
+
     describe '#resp' do
       it 'generates RESP response' do
         expect(subject.resp('PING')).to match(/PONG/)
